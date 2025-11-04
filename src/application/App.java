42375bc75d7c14120f8e8365ffa19d15ec78b894
Ricael <ricaelmenezes@gmail.com>
2025-11-04 19:35:19 -0300

    pc local

--- src/application/App.java
+++ src/application/App.java
@@ -1,12 +1,8 @@
 package application;
 
 
-import db.DB;
-
-import java.sql.Connection;
-
 public class App {
-	public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-	}
+    public static void main(String[] args) {
+
+    }
 }
--- src/dao/impl/ClienteDaoImpl.java
+++ src/dao/impl/ClienteDaoImpl.java
@@ -2,38 +2,61 @@ package dao.impl;
 
 import dao.ClienteDao;
 import db.DB;
+import exceptions.DataAccessException;
+import exceptions.ResourceNotFoundException;
 import model.entities.Cliente;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ClienteDaoImpl implements ClienteDao {
     @Override
     public Cliente procurarPorCPF(String cpf) {
-        try(Connection conn = DB.getConnection()){
+        try (Connection conn = DB.getConnection()) {
             PreparedStatement stmt = conn.prepareStatement("select * from cliente where cpf = ?");
             stmt.setString(1, cpf);
             ResultSet rs = stmt.executeQuery();
-            if(rs.next()){
-                Cliente cliente = new Cliente();
-                cliente.setIdCliente(rs.getLong("id"));
-                cliente.setNome(rs.getString("nome"));
-                cliente.setCpf(rs.getString("cpf"));
-                cliente.setTelefone(rs.getString("telefone"));
-                cliente.setEmail(rs.getString("email"));
-                return cliente;
+            if (rs.next()) {
+                return tabelaParaObjetoCliente(rs);
+            } else {
+                throw new ResourceNotFoundException();
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
         }
-        return null;
     }
 
     @Override
     public List<Cliente> procurarPorNome(String nome) {
-        return List.of();
+        try (Connection conn = DB.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("select * from cliente where nome like ?");
+            stmt.setString(1, "%" + nome + "%");
+            ResultSet rs = stmt.executeQuery();
+            List<Cliente> clientes = new java.util.ArrayList<>();
+            while (rs.next()) {
+                clientes.add(tabelaParaObjetoCliente(rs));
+            }
+            if (clientes.isEmpty()) {
+                throw new ResourceNotFoundException();
+            } else {
+                return clientes;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private Cliente tabelaParaObjetoCliente(ResultSet rs) throws SQLException {
+        Cliente cliente = new Cliente();
+        cliente.setIdCliente(rs.getLong("id"));
+        cliente.setNome(rs.getString("nome"));
+        cliente.setCpf(rs.getString("cpf"));
+        cliente.setTelefone(rs.getString("telefone"));
+        cliente.setEmail(rs.getString("email"));
+        return cliente;
     }
 
     @Override
